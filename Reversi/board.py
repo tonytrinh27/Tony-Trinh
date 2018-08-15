97ba062c0d8893eb9e0a20bd4e1ca540924c7119
TrinhThanhNhan <tonytrinhnhan@gmail.com>
2018-08-15 21:40:06 +0700

    ok

--- Reversi/board.py
+++ Reversi/board.py
@@ -1,6 +1,5 @@
 import reversi as bo
 
-player = "W"
 
 def creatboard(board):
     for i in bo.board:
@@ -28,60 +27,89 @@ def pointChoose(inputs, chr):
 
 
 def onBoard(x, y):
-    return x >= 0 and x <= 9 and y >= 0 and y <= 9
+    return x >= 1 and x < 9 and y >= 1 and y < 9
+
+
+def getplayer():
+    getlist = []
+    for i in range(1,9):
+        for j in range(1,9):
+            if bo.board[i][j] == "B":
+                getlist.append([j, i])
+            elif bo.board[i][j] == "W":
+                getlist.append([j, i])
+    return getlist
 
 
 def valid_choose(player):
+    for i in getplayer():
+        x = i[0]
+        y = i[1]
+        bo.board[x][y]
     if player == "W":
-        elplayer = 'B'
+        elplayer = "B"
     else:
         elplayer = "W"
-
+    replace_list = []
     list_Dot = []
-
-    for j in range(1, 9):
-        for i in range(1, 9):
-            if onBoard(j, i) and bo.board[i][j] == player:
-                for r, c in bo.direct:
-                    j += r
-                    i += c
-                    if not onBoard(j, i):
-                        continue
-                    if bo.board[i][j] == elplayer:
-                        j += r
-                        i += c
-                        if bo.board[i][j] == player:
-                            break
-                    if not onBoard(j, i):
-                        continue
-                    if bo.board[i][j] == player:
-                        break
-                    if bo.board[i][j] == ".":
-                        list_Dot.append([j,i])
-    return list_Dot
-
-a = valid_choose(player)
-
-def point(a):
     valid = set([])
-    for i in a:
+    for r, c in bo.direct:
+        x += c
+        y += r
+        if onBoard(x, y) and bo.board[x][y] == elplayer:
+            x += c
+            y += r
+            if not onBoard(x, y):
+                continue
+            if bo.board[x][y] == ".":
+                list_Dot.append([x, y])
+            while bo.board[x][y] == elplayer:
+                x += c
+                y += r
+                if not onBoard(x, y):
+                    break
+            if bo.board[x][y] == player:
+                while True:
+                    x -= c
+                    y -= r
+                    if x == i[0] and y == i[1]:
+                        break
+                    replace_list.append([x, y])
+        if onBoard(x, y) and bo.board[y][x] == ".":
+            list_Dot.append([x, y])
+        if onBoard(x, y) and bo.board[y][x] == player:
+            break
+    for i in list_Dot:
         for key, val in bo.dir.items():
             if i[0] == val:
                 valid.add(key+str(i[1]))
-    return valid
+
+
+    return ' '.join(valid), replace_list
+
+
+valid, replace = valid_choose("W")
+
+
+def flip(rep, chr):
+    for i in rep:
+        print(i)
+        bo.board[i[0]][i[1]] = player
 
 
 while 1:
     creatboard(bo.board)
-    print("Valid choices: ", point(a))
+    print("Valid choices: ",valid)
     playerW = input("Player W: ")
-    valid_choose(playerW)
+    valid_choose("W")
+    flip(replace, "W")
     chr, Elchr = playerChr(playerW)
     pointChoose(playerW, chr)
 
     creatboard(bo.board)
-    print("Valid choices: ", point(a))
+    print("Valid choices: ",valid)
     playerB = input("Player B: ")
-    valid_choose(playerB)
+    valid_choose("B")
+    flip(replace, "B")
     chr, Elchr = playerChr(playerB)
     pointChoose(playerB, Elchr)
